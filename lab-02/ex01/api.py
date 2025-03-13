05d4d5c4797b18b5b7696fb2cd0d553634625268
HuyNguyen191004 <huyva90@gmail.com>
2025-03-13 16:18:00 +0700

    Ngay2

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -1,23 +1,23 @@
 from flask import Flask, request, jsonify
-from cipher.caesar import CaesarCipher
+from cipher.railfence import RailFenceCipher
 app = Flask(__name__)
 
-caesar_cipher = CaesarCipher()
+railfence_cipher = RailFenceCipher()
 
-@app.route("/api/caesar/encrypt", methods = ["POST"])
-def caesar_encrypt():
+@app.route("/api/railfence/encrypt", methods = ["POST"])
+def railfence_encrypt():
     data = request.json
     plain_text = data['plain_text']
     key = int(data['key'])
-    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
     return jsonify({'encrypted_message': encrypted_text})
 
-@app.route("/api/caesar/decrypt", methods=["POST"])
-def caesar_decrypt():
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def railfence_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
     key = int(data['key'])
-    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
 if __name__ == "__main__":
