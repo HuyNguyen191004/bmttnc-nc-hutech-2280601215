bdffffc489fe0e73d37aca86c2c4ca7d98644670
HuyNguyen191004 <huyva90@gmail.com>
2025-04-03 11:21:51 +0700

    lab03

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -1,14 +1,29 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
+<<<<<<< HEAD
 app = Flask(__name__)
 
 caesar_cipher = CaesarCipher()
 
+=======
+from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
+app = Flask(__name__)
+
+caesar_cipher = CaesarCipher()
+vigenere_cipher = VigenereCipher()
+railfence_cipher = RailFenceCipher()
+#Caesar
+>>>>>>> 5a215985dc28da89f95ff561c017c341ffc46aba
 @app.route("/api/caesar/encrypt", methods = ["POST"])
 def caesar_encrypt():
     data = request.json
     plain_text = data['plain_text']
+<<<<<<< HEAD
     key = (data['key'])
+=======
+    key = int(data['key'])
+>>>>>>> 5a215985dc28da89f95ff561c017c341ffc46aba
     encrypted_text = caesar_cipher.caesar_encrypt(plain_text, key)
     return jsonify({'encrypted_message': encrypted_text})
 
@@ -17,9 +32,55 @@ def caesar_encrypt():
 def caesar_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
+<<<<<<< HEAD
     key = (data['key'])
     decrypted_text = caesar_cipher.caesar_decrypt(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+=======
+    key = int(data['key'])
+    decrypted_text = caesar_cipher.caesar_decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+#Vigenere
+
+@app.route("/api/vigenere/encrypt", methods = ["POST"])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = (data['key'])
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/vigenere/decrypt", methods=["POST"])
+def vigenere_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = (data['key'])
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+#RailFence
+
+@app.route("/api/railfence/encrypt", methods = ["POST"])
+def railfence_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def railfence_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    
+>>>>>>> 5a215985dc28da89f95ff561c017c341ffc46aba
--- lab-02/ex01/cipher/caesar/caesar_cipher.py
+++ lab-02/ex01/cipher/caesar/caesar_cipher.py
@@ -5,7 +5,11 @@ class CaesarCipher:
     def __init__(self):
         self.alphabet = ALPHABET
 
+<<<<<<< HEAD
     def encrypt_text(self, text:str,key: int)-> str:
+=======
+    def caesar_encrypt(self, text:str,key: int)-> str:
+>>>>>>> 5a215985dc28da89f95ff561c017c341ffc46aba
         alphabet_len = len(self.alphabet)
         text = text.upper()
         encrypted_text = []
@@ -16,7 +20,11 @@ class CaesarCipher:
             encrypted_text.append(output_lerter)
         return "".join(encrypted_text)
 
+<<<<<<< HEAD
     def decrypt_text(self, text: str,key: int) -> str:
+=======
+    def caesar_decrypt(self, text: str,key: int) -> str:
+>>>>>>> 5a215985dc28da89f95ff561c017c341ffc46aba
         alphabet_len = len(self.alphabet)
         text = text.upper()
         decrypted_text = []
@@ -25,4 +33,11 @@ class CaesarCipher:
             output_index = (lenter_index - key) % alphabet_len
             output_letter = self.alphabet[output_index]
             decrypted_text.append(output_letter)
+<<<<<<< HEAD
         return "".join(decrypted_text)
+=======
+        return "".join(decrypted_text)
+    
+    
+    
+>>>>>>> 5a215985dc28da89f95ff561c017c341ffc46aba
